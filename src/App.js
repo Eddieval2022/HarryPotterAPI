f413dfb8c2162a966defcb09dd59350bd66034af
Edward Valentine <eddieval2022@gmail.com>
2022-06-14 10:53:44 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CarPics = () => {
+  const [character, setCharacter] = useState([]);
+  const [error, setError] = useState (null);
+
+  useEffect(() => {
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch ('http://hp-api.herokuapp.com/api/characters'
+
+    );
+    if (!response.ok){
+      throw new Error (response.statusText);}
+      const data = await response.json();
+      console.log(data);
+      setCharacter(data);
+
+    }
+    catch (error){ console.log(error)
+   setError('could not find data');
+    }
+  
+  };
+  fetchData();
+}, []);
+  return ( 
+    
+<div className='Characters'>
+  {error && <p>{error}</p>}
+  <h1>Characters</h1>
+  {character.slice(0,25).map((char) => (
+    <div key = {char.id}>
+      <h2>Name : {char.name}</h2>
+       <img src={char.image} alt='car'/>
+      <p>Gender: {char.gender}</p>
+      <p>House: {char.house}</p>
+      <p>Species: {char.species}</p>
+      <p>D.O.B: {char.dateOfBirth}</p>
+     </div>
+   
+  ))}
+</div>
+   );
 }
+ 
+export default CarPics;
 
-export default App;
+
