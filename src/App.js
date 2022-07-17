f2103fbf4b933fd09d1955bb9c4c928ebca3890f
Edward Valentine <eddieval2022@gmail.com>
2022-07-17 21:29:50 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const CarPics = () => {
   
   };
   fetchData();
-}, []);
+}, []);  
   return ( 
     
 <div className='Characters'>
@@ -37,11 +37,13 @@ const CarPics = () => {
      and his friends Hermione Granger and Ron Weasley, all of whom are students at Hogwarts School of Witchcraft and Wizardry.
       The main story arc concerns Harry's struggle against Lord Voldemort, a dark wizard who intends to become immortal,
        overthrow the wizard governing body known as the Ministry of Magic and subjugate all wizards and Muggles (non-magical people).</p>
-       <h1>Characters</h1>
+       <p>The series follows the life of a boy named Harry Potter. In the first book, Harry Potter and the Philosopher's Stone, Harry lives in a cupboard under the stairs in the house of the Dursleys, his aunt, uncle and cousin. The Dursleys consider themselves perfectly normal, but at the age of eleven, Harry discovers that he is a wizard. He meets a half-giant named Hagrid who invites him to attend the Hogwarts School of Witchcraft and Wizardry. Harry learns that as a baby, his parents were murdered by the dark wizard Lord Voldemort. When Voldemort attempted to kill Harry, his curse rebounded and Harry survived with a lightning-shaped scar on his forehead.</p>
+       <p>Harry becomes a student at Hogwarts and is sorted into Gryffindor House. He gains the friendship of Ron Weasley, a member of a large but poor wizarding family, and Hermione Granger, a witch of non-magical, or Muggle, parentage. Harry encounters the school's potions master, Severus Snape, who displays a dislike for him; the rich pure-blood Draco Malfoy whom he develops an enmity with; and the Defence Against the Dark Arts teacher, Quirinus Quirrell, who turns out to be allied with Lord Voldemort. The first book concludes with Harry's confrontation with Voldemort, who, in his quest to regain a body, yearns to gain the power of the Philosopher's Stone, a substance that bestows everlasting life.</p>
+       <h2>Characters</h2>
   <div className='container'>
   {character.slice(0,25).map((char) => (
     <div className = 'info' key = {char.id}>
-      <h2>Name : {char.name}</h2>
+      <h3>Name : {char.name}</h3>
        <img src={char.image} alt='car'/>
       <p>Gender: {char.gender}</p>
       <p>House: {char.house}</p>
